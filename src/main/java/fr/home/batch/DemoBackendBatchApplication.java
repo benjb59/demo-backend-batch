707e83dc4f01ac3ec16389e3f74da45a42522c0c
Benjamin <benj.bour@gmail.com>
2021-02-16 09:09:05 +0100

    Update main

--- src/main/java/fr/home/batch/DemoBackendBatchApplication.java
+++ src/main/java/fr/home/batch/DemoBackendBatchApplication.java
@@ -3,8 +3,11 @@ package fr.home.batch;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import fr.home.batch.entities.Questionnaire;
+import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,12 +15,22 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class DemoBackendBatchApplication {
+public class DemoBackendBatchApplication implements CommandLineRunner{
 
 	private static final Logger log = LoggerFactory.getLogger(DemoBackendBatchApplication.class);
+
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoBackendBatchApplication.class, args);
@@ -28,15 +41,22 @@ public class DemoBackendBatchApplication {
 		return builder.build();
 	}
 
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate, ResourceLoader resourceLoader) throws Exception {
-		return args -> {
-			Resource res = resourceLoader.getResource("classpath:questionnaire.json");
-			String questionnaire = new BufferedReader(new InputStreamReader(res.getInputStream()))
-				.lines()
-					.reduce("", String::concat);
+	@Override
+	public void run(String... args) throws Exception {
 
-			restTemplate.postForEntity("https://demo-backend-mongo.dev.insee.io/developers", questionnaire, String.class);
-		};
+		Resource res = resourceLoader.getResource("classpath:questionnaire.json");
+		String questionnaireString = new BufferedReader(new InputStreamReader(res.getInputStream()))
+				.lines()
+				.reduce("", String::concat);
+
+		Questionnaire questionnaire = new Questionnaire();
+		questionnaire.setData(new JSONObject(questionnaireString));
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<Questionnaire> request = new HttpEntity<Questionnaire>(questionnaire, headers);
+
+		restTemplate.postForEntity("https://demo-backend-mongo.dev.insee.io/developers", request, String.class);
 	}
 }
